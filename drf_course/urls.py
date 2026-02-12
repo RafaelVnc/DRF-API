1f2ad232ee31d2a5364ea8e6df50fbbdf9864c22
Rafael Venancio <rafael.venancio08.06@gmail.com>
2026-02-12 12:35:54 -0300

    Api documentation features

--- drf_course/urls.py
+++ drf_course/urls.py
@@ -1,3 +1,4 @@
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import (
@@ -10,5 +11,9 @@ urlpatterns = [
     path('', include('api.urls')),
     path('silk/', include('silk.urls', namespace='silk')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),       
 ]
